3f94bda28ac870744622b7feeeaa7daf2a4cfbfb
jacklee.jabra@yahoo.com <evilc4tofficial@gmail.com>
2024-06-12 11:02:51 -0400

    Adding new while loop to scroll to the bottom of page as much as possible

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const axios = require('axios')
 const app = express()
 const puppeteer = require('puppeteer-extra')
-const pluginStealth = require('puppeteer-extra-plugin-stealth') 
-const {executablePath} = require('puppeteer'); 
+const pluginStealth = require('puppeteer-extra-plugin-stealth')
+const { executablePath } = require('puppeteer');
 const { v4: uuid } = require('uuid');
 const fs = require('fs')
 
@@ -27,27 +27,27 @@ app.listen(port = '3001', () => {
 app.get('/', (req, res) => {
 
 
-   res.render("home.ejs",{developer:'Jack Lee Jabra',license:'Opensource'})
+    res.render("home.ejs", { developer: 'Jack Lee Jabra', license: 'Opensource' })
 
 })
 
 app.post('/', (req, res) => {
 
-    const {format,url} = req.body
-    
-    if (format==='screenshot'){
+    const { format, url } = req.body
+
+    if (format === 'screenshot') {
 
         res.redirect(`/screenshot?url=${url}`)
 
-        
-    }else{
+
+    } else {
 
         res.redirect(`/pdf?url=${url}`)
 
     }
- 
- })
- 
+
+})
+
 
 
 // app.get('/form', (req, res) => {
@@ -65,15 +65,15 @@ app.get('/pdf', async (req, res) => {
     console.log('RECEIVED SOMETHING!')
     console.log(req.body)
     console.log(req.query)
-    
+
     const { url } = req.query
     console.log(url)
 
     // Use stealth 
-    puppeteer.use(pluginStealth()) 
+    puppeteer.use(pluginStealth())
 
     // Creates browser instance 
-    const browser = await puppeteer.launch({headless:"new"});
+    const browser = await puppeteer.launch({ headless: "new" });
 
     // Creates page instance
     const page = await browser.newPage();
@@ -87,7 +87,17 @@ app.get('/pdf', async (req, res) => {
         // Goes to URL and establishes page
         await page.goto(url, { waitUntil: 'networkidle0' });
 
-         
+        await page.waitForNavigation({
+            waitUntil: 'networkidle0',
+        });
+
+        await page.waitForSelector('section', { timeout: 5_000 });
+        await page.waitForSelector('div', { timeout: 5_000 });
+        await page.waitForSelector('p', { timeout: 5_000 });
+        await page.waitForSelector('a', { timeout: 5_000 });
+
+
+
 
         await page.emulateMediaType('screen');
 
@@ -106,8 +116,8 @@ app.get('/pdf', async (req, res) => {
             format: 'A4',
         });
 
-      
-        
+
+
         // Closes the browser session
 
         await browser.close();
@@ -117,24 +127,24 @@ app.get('/pdf', async (req, res) => {
 
 
         // res.render('result.ejs', { fileId, fileName })   
-        
 
-        
+
+
         // This uses the Filesystem module to delete the generated file after 10 seconds AFTER sending to the client
         // This prevents the server from being overloaded with thousands of unwanted files
-        setTimeout(()=>{
+        setTimeout(() => {
 
             fs.unlink(path.join(__dirname, `/pdfs/${fileName}`), function (err) {
                 if (err) throw err;
                 console.log(`File deleted!`);
-              });
+            });
 
-        },10000)
+        }, 10000)
 
 
     } catch (e) {
 
-        
+
         console.log('There Seems To Be An Error: ' + e)
         res.sendFile(path.join(__dirname, `/pdfs/default.pdf`));
 
@@ -156,15 +166,15 @@ app.get('/screenshot', async (req, res) => {
     console.log('RECEIVED SOMETHING!')
     console.log(req.body)
     console.log(req.query)
-    
+
     const { url } = req.query
     console.log(url)
 
     // Use stealth 
-    puppeteer.use(pluginStealth()) 
-    
+    puppeteer.use(pluginStealth())
+
     // Creates browser instance 
-    const browser = await puppeteer.launch({headless:"new"});
+    const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 
     // Creates page instance
     const page = await browser.newPage();
@@ -174,10 +184,76 @@ app.get('/screenshot', async (req, res) => {
     // If Link is not valid, it will redirect to the form submission via the catch 
     try {
 
+
         // Goes to URL and establishes page
+        console.log("Navigating to page...")
         await page.goto(url, { waitUntil: 'networkidle0' });
 
 
+        // THIS SCROLLS TO THE VERY BOTTOM OF THE PAGE
+        let prevHeight = -1;
+        let maxScrolls = 100;
+        let scrollCount = 0;
+
+        while (scrollCount < maxScrolls) {
+            // Scroll to the bottom of the page
+            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+            // Wait for page load
+            await page.waitForTimeout(1000);
+            // Calculate new scroll height and compare
+            let newHeight = await page.evaluate('document.body.scrollHeight');
+            if (newHeight == prevHeight) {
+                break;
+            }
+            prevHeight = newHeight;
+            scrollCount += 1;
+        }
+
+        const divSelectors = await page.evaluate(() => Array.from(document.querySelectorAll('div'), element => element.textContent));
+        const imgSelectors = await page.evaluate(() => Array.from(document.querySelectorAll('img'), element => element.textContent));
+        const videoSelectors = await page.evaluate(() => Array.from(document.querySelectorAll('video'), element => element.textContent));
+        const iframeSelectors = await page.evaluate(() => Array.from(document.querySelectorAll('iframe'), element => element.textContent));
+        const pSelectors = await page.evaluate(() => Array.from(document.querySelectorAll('p'), element => element.textContent));
+        const scriptSelectors = await page.evaluate(() => Array.from(document.querySelectorAll('script'), element => element.textContent));
+
+
+
+        console.log("Waiting For Elements...")
+        await page.waitForSelector('body');
+
+        if (divSelectors.length > 0) {
+            await page.waitForSelector('div', { timeout: 5_000 });
+            console.log("Divs Loaded...")
+        }
+        if (imgSelectors.length > 0) {
+            await page.waitForSelector('img', { timeout: 5_000 });
+            console.log("Images Loaded...")
+        }
+        if (videoSelectors.length > 0) {
+            await page.waitForSelector('video', { timeout: 5_000 });
+            console.log("Videos Loaded...")
+        }
+        if (iframeSelectors.length > 0) {
+            await page.waitForSelector('iframe', { timeout: 5_000 });
+            console.log("Iframes Loaded...")
+        }
+        if (pSelectors.length > 0) {
+            await page.waitForSelector('p', { timeout: 5_000 });
+            console.log("P's Loaded...")
+        }
+        if (scriptSelectors.length > 0) {
+            await page.waitForSelector('script', { timeout: 5_000 });
+            console.log("Scripts Loaded...")
+        }
+
+
+
+
+
+        // await page.waitForSelector('p', { timeout: 5_000 });
+        // await page.waitForSelector('a', { timeout: 5_000 });
+
+        console.log("Finished Waiting For Page Load.")
         await page.emulateMediaType('screen');
 
 
@@ -185,41 +261,67 @@ app.get('/screenshot', async (req, res) => {
         const fileId = uuid()
         const fileName = `result-${fileId}.png`
 
+        const wait = t => new Promise((resolve, reject) => setTimeout(resolve, t))
+
+        const generateScreenshot = async (id) => {
+
+            // Takes a Screenshot and saves under PATH
+            console.log("Generating Screenshot...")
+            await page.screenshot({
+
+
+                "type": "png", // can also be "jpeg" or "webp" (recommended)
+                "path": `./screenshots/result-${id}.png`,  // where to save it
+                "fullPage": true,  // will scroll down to capture everything if true
+
+            });
+
+            console.log("Screenshot Successfully Generated")
+
+            // Sends Screenshot to user
+            await res.sendFile(path.join(__dirname, `/screenshots/${fileName}`));
+
+
+        }
+
+
+        const deleteAndClose = async (file) => {
+
+            console.log("Deleting File...")
+            await fs.unlink(path.join(__dirname, `/screenshots/${fileName}`), function (err) {
+                if (err) throw err;
+                console.log(`File deleted!`);
+            });
+
+            // Closes the browser session
+            console.log("Closing Browser")
+            await browser.close();
+            console.log("Session Closed.")
+
+
+        }
+
+
+        await wait(10000)
+
+        await generateScreenshot(fileId)
+
+        await wait(10000)
+
+        await deleteAndClose(fileName)
 
 
 
-        // Takes a Screenshot and saves under PATH
-        await page.screenshot({
-            "type": "png", // can also be "jpeg" or "webp" (recommended)
-            "path": `./screenshots/result-${fileId}.png`,  // where to save it
-            "fullPage": true,  // will scroll down to capture everything if true
-        });
 
-
-        
-        // Closes the browser session
-
-        await browser.close();
-
-      
-        res.sendFile(path.join(__dirname, `/screenshots/${fileName}`));
 
 
         // This uses the Filesystem module to delete the generated file after 10 seconds AFTER sending to the client
         // This prevents the server from being overloaded with thousands of unwanted files
-        setTimeout(()=>{
-
-            fs.unlink(path.join(__dirname, `/screenshots/${fileName}`), function (err) {
-                if (err) throw err;
-                console.log(`File deleted!`);
-              });
-
-        },10000)
-
-        
 
 
-    }catch(e) {
+
+
+    } catch (e) {
 
         console.log('There Seems To Be An Error: ' + e)
         res.sendFile(path.join(__dirname, `/pdfs/default.pdf`));
